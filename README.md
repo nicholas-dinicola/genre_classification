bbd3fe55fad905b22e494c353a20fd61e3699cde
Nicholas Di Nicola <nicholasdinicola@MBP-di-Apple.home>
2021-09-25 16:53:59 +0200

    final ML pipeline

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # genre_classification
-Final excerice of the Udacity class on Machine LEarning Workflow 
+Final exercise of the CS Machine Learning Workflow. 
+
+Once logged in Weights and Biases (**$ wandb log in** in the command line)
+Run: **$  mlflow run . -P hydra_options="main.project_name=genre_classification_prod"**
+in the command line to execute the entire pipeline. 
--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def go(config: DictConfig):
         # This was passed on the command line as a comma-separated list of steps
         steps_to_execute = config["main"]["execute_steps"].split(",")
     else:
-        assert isinstance(config["main"]["execute_steps"], list)
-        steps_to_execute = config["main"]["execute_steps"]
+
+        steps_to_execute = list(config["main"]["execute_steps"])
 
     # Download step
     if "download" in steps_to_execute:
@@ -38,29 +38,26 @@ def go(config: DictConfig):
         )
 
     if "preprocess" in steps_to_execute:
-
         _ = mlflow.run(
-            os.path.join(root_path, "process"),
+            os.path.join(root_path, "preprocess"),
             "main",
             parameters={
                 "input_artifact": "raw_data.parquet:latest",
                 "artifact_name": "preprocessed_data.csv",
                 "artifact_type": "preprocessed_data",
-                "artifact_description": "Data preprocessed"
-            }
+                "artifact_description": "Data with preprocessing applied"
+            },
         )
 
     if "check_data" in steps_to_execute:
-
         _ = mlflow.run(
-            os.path.join(root_path, "check_data_data"),
+            os.path.join(root_path, "check_data"),
             "main",
             parameters={
-                # reference artifact needed for non-deterministic tests
                 "reference_artifact": config["data"]["reference_dataset"],
                 "sample_artifact": "preprocessed_data.csv:latest",
                 "ks_alpha": config["data"]["ks_alpha"]
-            }
+            },
         )
 
     if "segregate" in steps_to_execute:
@@ -70,21 +67,18 @@ def go(config: DictConfig):
             "main",
             parameters={
                 "input_artifact": "preprocessed_data.csv:latest",
-                # this component generates two artifacts, then it has a root as output artifact
                 "artifact_root": "data",
-                "artifact_type": "segregate_data",
+                "artifact_type": "segregated_data",
                 "test_size": config["data"]["test_size"],
                 "stratify": config["data"]["stratify"]
-            }
+            },
         )
 
     if "random_forest" in steps_to_execute:
-
         # Serialize decision tree configuration
         model_config = os.path.abspath("random_forest_config.yml")
 
         with open(model_config, "w+") as fp:
-            # take the rf params list from config file
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
         _ = mlflow.run(
@@ -95,20 +89,20 @@ def go(config: DictConfig):
                 "model_config": model_config,
                 "export_artifact": config["random_forest_pipeline"]["export_artifact"],
                 "random_seed": config["main"]["random_seed"],
-                "val_size": config["data"]["val_size"],
+                "val_size": config["data"]["test_size"],
                 "stratify": config["data"]["stratify"]
-            }
+            },
         )
 
     if "evaluate" in steps_to_execute:
 
         _ = mlflow.run(
-            uri=os.path.join(root_path, "evaluate"),
-            entry_point="mian",
+            os.path.join(root_path, "evaluate"),
+            "main",
             parameters={
                 "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
                 "test_data": "data_test.csv:latest"
-            }
+            },
         )
 
 
